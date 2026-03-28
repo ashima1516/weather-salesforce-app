6a04d0e1005c8c4c5483d7f7c5d58bee077cc733
ashima1516 <ashimabhatt725@gmail.com>
2026-03-28 16:12:44 +0530

    ui changed

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,28 +1,52 @@
+// import { LightningElement, track } from 'lwc';
+// import getWeather from '@salesforce/apex/WeatherService.getWeather';
+
+// export default class WeatherApp extends LightningElement {
+
+//     @track city;
+//     @track result;
+
+//     handleChange(event) {
+//         this.city = event.target.value;
+//     }
+
+//     getWeather() {
+
+//         if(!this.city){
+//             this.result = 'Please enter a city';
+//             return;
+//         }
+
+//         getWeather({ city: this.city })
+//             .then(data => {
+//                 this.result = data;
+//             })
+//             .catch(error => {
+//                 this.result = 'Error fetching data';
+//             });
+//     }
+// }
 import { LightningElement, track } from 'lwc';
 import getWeather from '@salesforce/apex/WeatherService.getWeather';
 
 export default class WeatherApp extends LightningElement {
 
-    @track city;
-    @track result;
+    city;
+    @track weatherData;
 
     handleChange(event) {
         this.city = event.target.value;
     }
 
     getWeather() {
-
-        if(!this.city){
-            this.result = 'Please enter a city';
-            return;
-        }
-
+         console.log('Button clicked'); 
         getWeather({ city: this.city })
             .then(data => {
-                this.result = data;
+                 console.log('DATA:', data);
+                this.weatherData = data;
             })
             .catch(error => {
-                this.result = 'Error fetching data';
+                console.error('ERROR:', error);
             });
     }
 }
